9662a7c083cc4faaed90965609129c3fa1250393
azertypow <azertypow@gmail.com>
2022-03-03 22:15:34 +0100

    fix: git project repository can't push

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: 'docs',
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? 'https://theiviproject.github.io/website/'
+    : '/',
 })
